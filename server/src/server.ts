c2e91f955e5ee53e2549ff3c8b9de4845fa3b630
Daniele Carnaúba <dannygoncalves41@gmail.com>
2023-01-18 01:10:31 -0300

    refactor: changing the link folder with the bd

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,8 @@
 import Fastify from "fastify";
 import cors from "@fastify/cors";
-import { PrismaClient } from "@prisma/client";
+import { prisma } from "@prisma/client";
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
 
