1c51b6e766e06b26c598fc46a9ecbe5ad3ffecf4
Daniele Carnaúba <dannygoncalves41@gmail.com>
2023-01-20 23:10:18 -0300

    chore: add clsx

--- web/src/components/HabitDay.tsx
+++ web/src/components/HabitDay.tsx
@@ -1,11 +1,14 @@
 import * as Popover from '@radix-ui/react-popover';
 import { ProgressBar } from './ProgressBar';
 
-//interface HabitDayProps {
-//  completed: number
-//}
+interface HabitDayProps {
+  completed: number
+  amount: number
+}
 
-export function HabitDay() {
+export function HabitDay( {completed, amount} : HabitDayProps) {
+  const completedPercentage = Math.round((completed / amount) * 100)
+
   return (
     <Popover.Root>
       <Popover.Trigger className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg" />
@@ -15,7 +18,7 @@ export function HabitDay() {
           <span className='font-semibold text-zinc-400'>segunda-feira</span>
           <span className='mt-1 font-extrabold leading-tight text-3xl'>20/01</span>
 
-          <ProgressBar progress={50}/>
+          <ProgressBar progress={completedPercentage}/>
 
 
           <Popover.Arrow height={8} width={16} className="fill-zinc-900"/>
