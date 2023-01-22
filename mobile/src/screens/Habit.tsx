34a0190442caa18890060129eb7aa506dceffd06
Daniele Carnaúba <dannygoncalves41@gmail.com>
2023-01-22 19:16:46 -0300

    feat: disable changing habits in the past

--- mobile/src/screens/Habit.tsx
+++ mobile/src/screens/Habit.tsx
@@ -9,6 +9,7 @@ import { Loading } from "../components/Loading";
 import { api } from "../lib/axios";
 import { generateProgressPercentage } from '../../utils/generate-progess-percentage'
 import { HabitEmpty } from "../components/HabitsEmpty";
+import clsx from "clsx";
 
 interface Params {
     date: string;
@@ -32,6 +33,7 @@ export function Habit() {
     const { date } = route.params as Params;
 
     const parsedDate = dayjs(date);
+    const isDateInPast = parsedDate.endOf('day').isBefore(new Date());
     const dayOfWeek = parsedDate.format('dddd');
     const dayAndMonth = parsedDate.format('DD/MM');
 
@@ -91,7 +93,9 @@ export function Habit() {
 
                 <ProgressBar progress={habitsProgress}/>
 
-                <View className="mt-6">
+                <View className={clsx("mt-6", {
+                    'opacity-50': isDateInPast
+                })}>
                     {
                         dayInfo?.possibleHabits ?
                         dayInfo?.possibleHabits.map(habit => (
@@ -99,6 +103,7 @@ export function Habit() {
                                 key={habit.id}
                                 title={habit.title}
                                 checked={completedHabits.includes(habit.id)}
+                                disabled={isDateInPast}
                                 onPress= {() => handleToggleHabit(habit.id)}
                             />
                         ))
@@ -107,7 +112,15 @@ export function Habit() {
                     }
                     
                 </View>
-
+                {
+                    isDateInPast && (
+                        <Text
+                            className="text-white mt-10 text-center"
+                        >
+                            Você não pode atualizar hábitos no passado.
+                        </Text>
+                   )
+                }
             </ScrollView>
         </View>
     )
